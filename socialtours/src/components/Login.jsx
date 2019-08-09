f5ca4930baf4b9335aa4f34e3a7e8c608b31ddcc
Louis Magdaleno <buildthatapp@gmail.com>
2019-08-08 22:18:49 -0700

    Added login / signup component

---
+++ socialtours/src/components/Login.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+const axios = require('axios');
+
+class Login extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isLoggingIn : false,
+            isLoggedIn: false,
+            user: {
+                first_name: '',
+                last_name: '',
+                email: '',
+                password: ''
+            },
+            response: {}
+        }
+    }
+
+    render(){
+        return (
+            <>
+                {this.state.isLoggedIn ? <h2>Congrats! You're logged in.</h2> : <></>}
+                <form style={{width: '300px', height: '200px', display: 'flex', flexDirection: 'column'}} onSubmit={this.state.isLoggingIn ? this.login : this.signup}>
+                    {this.state.isLoggingIn ? <>
+                        <input style={{width: '50%'}} name='first_name' value={this.state.user.first_name} placeholder='First Name' onChange={this.handleChange} />
+                        <input style={{width: '50%'}} name='last_name' value={this.state.user.last_name} placeholder='Last Name' onChange={this.handleChange} />
+                        </>
+                        : <></>}
+                    <input style={{width: '50%'}} name='email' value={this.state.user.email}  placeholder='Email' onChange={this.handleChange} />
+                    <input style={{width: '50%'}} name='password' value={this.state.user.password} placeholder='Password' onChange={this.handleChange} />
+                    {!this.state.isLoggingIn ? <span style={{width: '50%', outline: '1px solid black', textAlign: 'center'}} onClick={() => this.setState({isLoggingIn : !this.state.isLoggingIn})}>Sign Up</span> : <></>}
+                    <button style={{width: '50%'}} type='submit' value='submit' >{!this.state.isLoggingIn ? 'Log In' : 'Lets do this' }</button>
+                    
+                </form>
+            </>
+        )
+    }
+
+    handleChange = event => {
+        this.setState({user: {value: event.target.value}});
+      }
+    
+    handleSubmit = event =>{
+        event.preventDefault();
+        if (this.state.isLoggingIn){
+            if (this.isValidLogIn){
+                this.login();
+            }   
+        } else {
+            if (this.isValidSignUp) {
+                this.signup();
+            }   
+        }
+      }
+
+    isValidSignUp = () => {
+        const {first_name, last_name, email, password} = this.state.user;
+
+        if (first_name.length > 0 &&
+            last_name.length > 0 &&
+            email.length > 0 &&
+            password.length > 0){
+                return true;
+        } else {
+                return false;
+            }
+    }
+
+    isValidLogIn= () => {
+        const {email, password} = this.state.user;
+
+        if (email.length > 0 && password.length > 0){
+                return true;
+        } else {
+                return false;
+            }
+    }
+
+    login = () => {
+
+        if (this.isValidLogIn){
+            const {email, password} = this.state.user;
+            const user = {email, password};
+            axios.post('www.staging-a-socialtours.heroku.com/api/login', user)
+            .then(res => 
+                this.setState({response: {...res.data}, isLoggedIn: true}))
+            .catch(err => console.log(err))
+        }
+            
+    }
+
+    signup = () => {
+        const user = {...this.state.user};
+
+        axios.post('www.staging-a-socialtours.heroku.com/api/register', user)
+        .then(res => 
+            console.log('sign up successful , do something'))
+        .catch(err => console.log(err))
+    }
+}
+
+export default Login;
