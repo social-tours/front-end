f97c470d776a47d4234a2e6b70f8622fe1930c03
adam <bcuz@users.noreply.github.com>
2019-08-13 23:00:20 -0400

    add clarifying text

--- socialtours/src/App.js
+++ socialtours/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div>
         <p>
-          {this.state.usersData.length}
+          {this.state.usersData.length} users in database
         </p>
       </div>
     );
