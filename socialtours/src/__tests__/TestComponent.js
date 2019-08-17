14a86b171ae81e02a0dcc447599bd560cfffafc1
Rane <github@blasph.me>
2019-08-16 19:16:33 -0700

    flipped example tests

--- socialtours/src/__tests__/TestComponent.js
+++ socialtours/src/__tests__/TestComponent.js
@@ -4,6 +4,7 @@
  */
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import TestComponent from '../components/TestComponent';
 
@@ -16,15 +17,15 @@ describe('Example tests to verify integration testing works', () => {
         const { getByTestId } = render(<TestComponent/>);
         const h1Area = getByTestId('h1-area');
 
-        expect(h1Area.toHaveTextContent('default'));
+        expect(h1Area).toHaveTextContent('default');
     });
     it('changes the message when the button is clicked', () => {
-        const { getByTestId, getByText } = render(<TestComponent/>);
+        const {getByTestId, getByText} = render(<TestComponent/>);
         const h1Area = getByTestId('h1-area');
         const changeMessageButton = getByText('Change Message');
 
         fireEvent.click(changeMessageButton);
 
-        expect(h1Area.toHaveTextContent('test message'));
+        expect(h1Area).toHaveTextContent('test message');
     });
-});
+})
