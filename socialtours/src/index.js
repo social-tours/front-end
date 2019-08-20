7a9c767779d34447c09dd9c6a941de9cef70708a
Rane <github@blasph.me>
2019-08-19 19:13:18 -0700

    Added Redux support
    Installed Redux, React-Redux, and Thunk to support Redux use in the
    app. Created an initial (empty) reducer and imported into index.js
    Wrapped Provider around app.

--- socialtours/src/index.js
+++ socialtours/src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from "./reducers";
+import "./index.css";
+import App from "./App";
+
+const store = createStore(rootReducer, applyMiddleware(null));
+
+ReactDOM.render(<App />, document.getElementById("root"));
