0e68d9cfb7fdad22aaca5651f93e5a948ca44105
Rane <github@blasph.me>
2019-08-19 20:15:26 -0700

    Merge branch 'master' of https://github.com/social-tours/front-end
    # Conflicts:
    #	socialtours/src/App.js
    #	socialtours/src/index.js

--- socialtours/src/App.js
+++ socialtours/src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import axios from "axios";
-import API_ENDPOINT from './config/api';
+import API_ENDPOINT from "./config/api";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usersData: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            usersData: [],
+        };
+    }
 
-  componentDidMount() {
-    axios
-      .get(`${API_ENDPOINT}/api/users`)
+    componentDidMount() {
+        axios
+            .get(`${API_ENDPOINT}/api/users`)
 
-      .then(response => {
-        this.setState({ usersData: response.data });      
-      })
-      .catch(err => console.log(err));
-  }
+            .then(response => {
+                this.setState({ usersData: response.data });
+            })
+            .catch(err => console.log(err));
+    }
 
-  render() {
-    return (
-      <div>
-        <p>
-          {this.state.usersData.length} users in database
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <p>{this.state.usersData.length} users in database</p>
+            </div>
+        );
+    }
 }
 export default App;
