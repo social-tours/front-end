2446d671795185cefd843a31972d74225aa1ce94
Rane <github@blasph.me>
2019-08-19 19:23:34 -0700

    Fixing some typos with Redux configuration

--- socialtours/src/index.js
+++ socialtours/src/index.js
@@ -8,11 +8,11 @@ import rootReducer from "./reducers";
 import "./index.css";
 import App from "./App";
 
-const store = createStore(rootReducer, applyMiddleware(null));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
--- socialtours/src/reducers/index.js
+++ socialtours/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 
 export default combineReducers({
-  default: () => {}
+    default: (state = {}) => {
+        return state;
+    },
 });
