ac914770d21c6f7328b85413977522632b9a85c6
Rane <github@blasph.me>
2019-08-21 20:48:10 -0700

    Created tests for calendar display

--- socialtours/src/__tests__/EventCalendar.js
+++ socialtours/src/__tests__/EventCalendar.js
@@ -1,15 +1,50 @@
 import React from "react";
-import { getByTestId } from "@testing-library/dom";
-import { render } from "@testing-library/react";
+import { getByTestId, getByText } from "@testing-library/dom";
+import { render, fireEvent } from "@testing-library/react";
 
 import EventCalendar from "../components/EventCalendar";
 
+const MONTHS = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
 describe("EventCalendar display", () => {
+	let currentMonth;
+	beforeEach(() => {
+		currentMonth = new Date().getMonth();
+	});
 	it("Loads the component", () => {
 		const { getByTestId } = render(<EventCalendar />);
+		const calendarComponent = getByTestId("component-calendar");
 	});
-	it("Loads the current month's calendar", () => {});
-	it("Navigates through calendar months", () => {});
+	it("Loads the current month's calendar", () => {
+		const { getByText } = render(<EventCalendar />);
+		getByText(MONTHS[currentMonth]);
+	});
+	it("Navigates through calendar months", () => {
+		const { getByText } = render(<EventCalendar />);
+		const getNextMonth = getByText(">");
+		const getLastMonth = getByText("<");
+		const nextMonth = MONTHS[currentMonth + 1];
+		const lastMonth = MONTHS[currentMonth - 1];
+
+		fireEvent.click(getNextMonth);
+		getByText(nextMonth);
+
+		fireEvent.click(getLastMonth);
+		fireEvent.click(getLastMonth);
+	});
 });
 
 describe("EventCalendar functionality", () => {
