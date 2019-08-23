f449ecfcd91b06ed890c77e89f7ec68ca9c330d4
Rane <github@blasph.me>
2019-08-22 20:33:04 -0700

    Added react-calendar component
    Passing view and navigation tests.

--- socialtours/src/__tests__/EventCalendar.js
+++ socialtours/src/__tests__/EventCalendar.js
@@ -21,30 +21,52 @@ const MONTHS = [
 
 describe("EventCalendar display", () => {
 	let currentMonth;
+	let currentYear;
 	beforeEach(() => {
 		currentMonth = new Date().getMonth();
+		currentYear = new Date().getFullYear();
 	});
 	it("Loads the component", () => {
 		const { getByTestId } = render(<EventCalendar />);
 		const calendarComponent = getByTestId("component-calendar");
 	});
 	it("Loads the current month's calendar", () => {
-		const { getByText } = render(<EventCalendar />);
-		getByText(MONTHS[currentMonth]);
+		const { container } = render(<EventCalendar />);
+		const month = container.querySelector(".react-calendar__navigation__label")
+			.firstChild.nodeValue;
+
+		expect(month).toBe(MONTHS[currentMonth] + " " + currentYear);
 	});
 	it("Navigates through calendar months", () => {
-		const { getByText } = render(<EventCalendar />);
-		const getNextMonth = getByText(">");
-		const getLastMonth = getByText("<");
+		let monthYear;
+		const { container } = render(<EventCalendar />);
+		const getLastMonth = container.querySelector(
+			".react-calendar__navigation__prev-button"
+		).firstChild;
+
+		const getNextMonth = container.querySelector(
+			".react-calendar__navigation__next-button"
+		).firstChild;
+
+		console.log(getLastMonth);
+
 		const nextMonth = MONTHS[currentMonth + 1];
 		const lastMonth = MONTHS[currentMonth - 1];
 
 		fireEvent.click(getNextMonth);
-		getByText(nextMonth);
+
+		monthYear = container.querySelector(".react-calendar__navigation__label")
+			.firstChild.nodeValue;
+
+		expect(monthYear).toBe(nextMonth + " " + currentYear);
 
 		fireEvent.click(getLastMonth);
 		fireEvent.click(getLastMonth);
-		getByText(lastMonth);
+
+		monthYear = container.querySelector(".react-calendar__navigation__label")
+			.firstChild.nodeValue;
+
+		expect(monthYear).toBe(lastMonth + " " + currentYear);
 	});
 });
 
--- socialtours/src/components/EventCalendar.js
+++ socialtours/src/components/EventCalendar.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
+import Calendar from "react-calendar";
+
 class EventCalendar extends Component {
-    render() {
-        return <div />;
-    }
+	render() {
+		return (
+			<div data-testid="component-calendar">
+				<Calendar />
+			</div>
+		);
+	}
 }
 
 export default EventCalendar;
