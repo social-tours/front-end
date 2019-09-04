c3e94daefcd57ca0af3a20cce7ab00632f6d284b
James Wigdor <jpwigdor@gmail.com>
2019-09-04 19:31:07 -0400

    Scaffolded Manage Events file

---
+++ socialtours/src/components/ManageEvents.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ManageEvents extends Component {
+	render() {
+		return (
+			<>
+				<p>Mangage Events</p>
+
+				<div className="Left-Container"></div>
+
+				<div className="Right-Container"></div>
+			</>
+		);
+	}
+}
+
+export default connect()();
