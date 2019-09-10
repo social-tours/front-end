b18fa92a7516a10b508bb5c458785dd2821ef2b5
Michael Jenkins <mjenkins9605@vidangel.com>
2019-09-09 23:30:38 -0400

    create event is working

--- socialtours/src/components/createEvent.js
+++ socialtours/src/components/createEvent.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { connect } from "react-redux";
-// import axios from 'axios'; // for myTestEventPost
 
 import { fetchEvent, postEvent } from '../actions/index.js'
-// const API = 'https://staging-a-socialtours.herokuapp.com' // for myTestEventPost
 
 class TheCreateEvent extends React.Component {
     state = {
-        id: '',
         type: 1, // hardcoded for testing
         title: '',
         host_id: 2, //FK to user_ID, hardcoded for now to get to work
@@ -28,20 +25,10 @@ class TheCreateEvent extends React.Component {
         this.props.postEvent(newEvent);
     }
 
-    // myTestEventPost = async () => {// to test the API, not plugged in when working
-    //     const testEvent = {
-    //         ...this.state
-    //     }
-    //     const myFunction = await axios.post(API + "/api/events", testEvent)
-    //     console.log(myFunction)
-    // }
-
     render() {
         return (
             <form >
-                <input name='type' placeholder='type' onChange={this.handleChange} value={this.state.type} type='number' />
                 <input name='title' placeholder='title' onChange={this.handleChange} value={this.state.title} />
-                <input name='host_ID' placeholder='host_ID' onChange={this.handleChange} value={this.state.host_ID} />
                 <input name='description' placeholder='description' onChange={this.handleChange} value={this.state.description} />
                 <input name='event_image' placeholder='event_image' onChange={this.handleChange} value={this.state.event_image} />
                 <input name='capacity' placeholder='capacity' onChange={this.handleChange} value={this.state.capacity} type='number' />
@@ -61,15 +48,3 @@ export default connect(
     mapStateToProps,
     { fetchEvent, postEvent }
 )(TheCreateEvent);
-
-
-// ** Event Details - Objects in WireFrame ** 
-// Wir
-// Date Of Event - Where is Date Of Event? Didnt Wilfred just add to DB?
-// Event Type - 
-// Location - Location is not in the DB but is in the wireframe for Event Details
-// Topic/ Event Name 
-// Event Image
-// Shareable Social Media Link
-// Event Description
-// Event and Delete buttons
