3098c22d3a7b0a67eac5ac5840b31593ddce564c
Michael Jenkins <mjenkins9605@vidangel.com>
2019-09-23 23:54:28 -0400

    removed original idea

--- socialtours/src/components/AboutUsContact.js
+++ socialtours/src/components/AboutUsContact.js
@@ -1,92 +1,13 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { withStyles } from "@material-ui/core/styles";
-// import Card from "@material-ui/core/Card";
-// import CardMedia from "@material-ui/core/CardMedia";
-// import CardContent from "@material-ui/core/CardContent";
-// import Divider from "@material-ui/core/Divider";
-// import Typography from "@material-ui/core/Typography";
-
-// import "./styles.css";
-
-
-// const styles = aboutTheme => ({
-//   card: {
-//     maxWidth: 300,
-//     margin: "auto",
-//     transition: "0.3s",
-//     boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-//     "&:hover": {
-//       boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
-//     }
-//   },
-//   media: {
-//     paddingTop: "56.25%"
-//   },
-//   content: {
-//     textAlign: "left",
-//     padding: aboutTheme.spacing.unit * 3
-//   },
-//   divider: {
-//     margin: `${aboutTheme.spacing.unit * 3}px 0`
-//   },
-//   heading: {
-//     fontWeight: "bold"
-//   },
-//   subheading: {
-//     lineHeight: 1.8
-//   },
-//   avatar: {
-//     display: "inline-block",
-//     border: "2px solid white",
-//     "&:not(:first-of-type)": {
-//       marginLeft: -aboutTheme.spacing.unit
-//     }
-//   }
-// });
-
-// class AboutUsContact extends React.Component {
-
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h2>The Crew</h2>
-//         <Card className={classes.card}>
-//           <CardMedia className={classes.media} image={"../images/gannon.png"} />
-//           <CardContent className={classes.content}>
-//             <Typography className={"AboutName"} variant={"h6"} gutterBottom> Gannon Darcy </Typography>
-//             <Typography className={"AboutTitle"} variant={"h8"}> Full-Stack Developer </Typography>
-//             <Divider className={classes.divider} light />
-//           </CardContent>
-//         </Card>
-//       </div>
-//     );
-//   }
-// }
-// const WrappedApp = withStyles(styles)(App);
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<WrappedApp />, rootElement);
-
-// export default AboutUsContact;
-
-
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 
 
 
