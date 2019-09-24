3fe1ffc2ad8a97467965a0231b705e7f8d321255
adam <bcuz@users.noreply.github.com>
2019-09-23 22:41:17 -0400

    render empty Credits component

---
+++ socialtours/src/components/Credits.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Credits = () => {
+	return (
+		<Wrapper>
+			<p>hi</p>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div`
+	margin-top: 35px;
+`;
+
+export default Credits;
