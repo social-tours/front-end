59a30f98b0301cae505223dc8310ef83591dab0e
James Wigdor <jpwigdor@gmail.com>
2019-10-03 08:46:54 -0400

    Add Subscription Reducer

---
+++ socialtours/src/reducers/subscriptionReducer.js
@@ -0,0 +1,25 @@
+import { types } from "../actions/subscriptionActions";
+
+const initialState = {
+    fetchingSubscription: false,
+    subscriptionError: false,
+    subscriptions: []
+}
+
+const subscriptionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.FETCHING_SUBSCRIPTION:
+            return { ...state, fetchingSubscription: true };
+        case types.FETCH_SUBSCRIPTION_FAILED:
+            return { ...state, fetchingSubscription: false, subscriptionError: true };
+        case types.FETCH_SUBSCRIPTION_SUCCESS:
+            return { ...state, 
+                fetchingSubscription: false, 
+                subscriptionError: false, 
+                subscriptions: action.payload};
+        default:
+            return state;
+    }
+}
+
+export default subscriptionReducer;
