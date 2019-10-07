29eafafd3a192a2d728c82ebc0a44f6452630876
Louis Magdaleno <buildthatapp@gmail.com>
2019-10-06 19:49:01 -0700

    Added fetchResults method and search box / submit btn

--- socialtours/src/components/Search.js
+++ socialtours/src/components/Search.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import axios from 'axios';
+import API_ENDPOINT from '../config/api';
 
 class Search extends React.Component {
 	state = {
-		results: []
+        results: [],
+        search: ''
 	};
 
 	componentDidUpdate(prevProps, prevState, snapshot) {
@@ -14,16 +17,51 @@ class Search extends React.Component {
 		}
 	}
 
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    fetchResults = _ => {
+        axios.get(API_ENDPOINT)
+        .then(res => {
+            this.setState({
+                results : res.data
+            })
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    };
+
 	render() {
 		if (this.state.results.length === 0)
 			return (
-				<SearchBar />
+				<Section>
+                    <SearchBar />
+                    <SearchResults placeholder="Search for an influencer"
+                                name="search"
+                                value={this.state.search}
+                                onChange={this.handleChange} 
+                    />
+                    <SearchBtn onClick={this.fetchResults}>Search</SearchBtn>
+                </Section>
 			);
 
 		return (
             <Section>
                 <SearchBar />
-                <SearchResults />
+                <SearchResults placeholder="Search for an influencer"
+                               name="search"
+                               value={this.state.search}
+                               onChange={this.handleChange} 
+                />
+                <SearchBtn onClick={this.fetchResults}>Search</SearchBtn>
+                <SearchResults >
+                    {this.state.results && this.state.results.map(result => <SearchResult {...result} />)}
+                </SearchResults>
             </Section>
 		);
 	}
@@ -31,5 +69,8 @@ class Search extends React.Component {
 
 const Section = styled.div``;
 const SearchBar = styled.input``;
+const SearchBtn = styled.span``;
+const SearchResults = styled.div``;
+const SearchResult = styled.div``;
 
 export default Search;
