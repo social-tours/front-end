dfa639804e50595f94bf1bf0aa06c597332f565e
adam <bcuz@users.noreply.github.com>
2019-10-09 11:15:36 -0400

    prettier

--- socialtours/src/reducers/subscriptionReducer.js
+++ socialtours/src/reducers/subscriptionReducer.js
@@ -1,25 +1,27 @@
 import { types } from "../actions/subscriptionActions";
 
 const initialState = {
-    fetchingSubscription: false,
-    subscriptionError: false,
-    subscriptions: []
-}
+	fetchingSubscription: false,
+	subscriptionError: false,
+	subscriptions: []
+};
 
 const subscriptionReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case types.FETCHING_SUBSCRIPTION:
-            return { ...state, fetchingSubscription: true };
-        case types.FETCH_SUBSCRIPTION_FAILED:
-            return { ...state, fetchingSubscription: false, subscriptionError: true };
-        case types.FETCH_SUBSCRIPTION_SUCCESS:
-            return { ...state, 
-                fetchingSubscription: false, 
-                subscriptionError: false, 
-                subscriptions: action.payload};
-        default:
-            return state;
-    }
-}
+	switch (action.type) {
+		case types.FETCHING_SUBSCRIPTION:
+			return { ...state, fetchingSubscription: true };
+		case types.FETCH_SUBSCRIPTION_FAILED:
+			return { ...state, fetchingSubscription: false, subscriptionError: true };
+		case types.FETCH_SUBSCRIPTION_SUCCESS:
+			return {
+				...state,
+				fetchingSubscription: false,
+				subscriptionError: false,
+				subscriptions: action.payload
+			};
+		default:
+			return state;
+	}
+};
 
 export default subscriptionReducer;
