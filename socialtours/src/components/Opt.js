264ff88979f59d73ff51aa84c86b3f0d3da3257a
adam <bcuz@users.noreply.github.com>
2019-10-08 22:47:58 -0400

    render influencers

--- socialtours/src/components/Opt.js
+++ socialtours/src/components/Opt.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSubscriptions } from "../actions/subscriptionActions";
 
 class Opt extends Component {
+	componentDidMount() {
+		this.props.getSubscriptions();
+	}
+
 	render() {
-		return <h1 style={{ marginTop: "40px" }}>hi</h1>;
+		return (
+			<ul style={{ marginTop: "40px" }}>
+				{this.props.subscriptions.map(sub => (
+					<li>{sub.influencer_name}</li>
+				))}
+			</ul>
+		);
 	}
 }
 
-export default Opt;
+const mapStateToProps = state => {
+	console.log("ManageEvents State: ", state);
+	return {
+		subscriptions: state.subscriptionReducer.subscriptions
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{ getSubscriptions }
+)(Opt);
