0a82a1dec59d839fdca9436272f533d8a40563c5
GregoryUrban <turknben@gmail.com>
2019-11-05 23:59:41 -0500

    Added Create New Event Button

--- socialtours/src/components/ManageEvents/EventsDashboard.js
+++ socialtours/src/components/ManageEvents/EventsDashboard.js
@@ -8,6 +8,7 @@ import Panel from "./Panel";
 import Tabs from "./Tabs";
 import * as S from "./EventFormStyles";
 
+{/* Drop Down Schedule imports and styling start*/ }
 import { makeStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
@@ -15,127 +16,150 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
+import {
+    EventContainer,
+    EventsWrapper,
+    EventsContainerContainer,
+    EventHeader,
+    EventsTitle,
+    EventSectionTitle,
+    ManageEventsSub,
+    ManageEventsWrapper,
+    EventsSection,
+    EventsHeadline,
+    CEButton
+} from "./ManageEventsStyles";
+
 const classes = makeStyles(theme => ({
-	root: {
-		width: "100%"
-	},
-	heading: {
-		fontSize: theme.typography.pxToRem(15),
-		fontWeight: theme.typography.fontWeightRegular
-	}
+    root: {
+        width: "100%"
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular
+    }
 }));
 
+{/* Drop Down Schedule imports and styling end*/ }
+
 class EventsDashboard extends Component {
-	state = {
-		userId: null
-	};
+    state = {
+        userId: null
+    };
 
-	handleFetchEvents() {
-		this.props.fetchEvents();
-	}
+    handleFetchEvents() {
+        this.props.fetchEvents();
+    }
 
-	handleFetchSubscriptions() {
-		this.props.getSubscriptionsByUserId();
-	}
+    handleFetchSubscriptions() {
+        this.props.getSubscriptionsByUserId();
+    }
 
-	getUserId() {
-		if (localStorage.getItem("api_token")) {
-			return jwtDecode(localStorage.getItem("api_token"));
-		}
-	}
+    getUserId() {
+        if (localStorage.getItem("api_token")) {
+            return jwtDecode(localStorage.getItem("api_token"));
+        }
+    }
 
-	componentDidMount() {
-		document.title = "Events Dashboard | Social Tours";
-		this.handleFetchEvents();
-		this.handleFetchSubscriptions();
-		const userId = this.getUserId().id;
-		this.setState({ userId });
-	}
+    componentDidMount() {
+        document.title = "Events Dashboard | Social Tours";
+        this.handleFetchEvents();
+        this.handleFetchSubscriptions();
+        const userId = this.getUserId().id;
+        this.setState({ userId });
+    }
 
-	render() {
-		return (
-			<S.DashBoardContainer>
-				<Tabs>
-					<Panel title="My Events">
-						{this.props.events
-							.filter(event => event.host_id === this.state.userId)
-							.map(event => (
-								<Link key={event.id} to={`/events/${event.id}`}>
+    render() {
+        return (
+            <S.DashBoardContainer>
+                <Tabs>
+                    <Panel title="My Events">
 
-									{/* Added in to show a dropdown of the events' schedules' */}
-									<S.Preview key={event.id}>{event.title}
-									<div className={classes.root}>
-										<ExpansionPanel expanded={props.expanded}>
-											<ExpansionPanelSummary
-												expandIcon={<ExpandMoreIcon />}
-												id={`${props.id}_header`}
-											>
-												<Typography className={classes.heading}>{props.title}</Typography>
-											</ExpansionPanelSummary>
-											<ExpansionPanelDetails>
-												<div>
-													{props.schedule &&
-														props.schedule.length > 0 &&
-														props.schedule.map(schd => {
-															let date = new Date(schd.start_date_time);
-															return (
-																<ScheduleWrapper>
-																	<span>{`${schd.location} ${moment(date).format(
-																		"MMMM Do YYYY, h:mm:ss a"
-																	)}`}</span>
-																</ScheduleWrapper>
-															);
-														})}
-												</div>
-											</ExpansionPanelDetails>
-										</ExpansionPanel>
-									</div>
-									</S.Preview>
+                        {this.props.events
+                            .filter(event => event.host_id === this.state.userId)
+                            .map(event => (
+                                <Link key={event.id} to={`/events/${event.id}`}>
+                                    <S.Preview key={event.id}>{event.title}
 
 
-								</Link>
-							))}
-					</Panel>
-					<Panel title="Influencer Events">
-						{this.props.events
-							.filter(event =>
-								this.props.subscriptions
-									.map(sub => sub.influencer_id)
-									.includes(event.host_id)
-							)
-							.map(event => (
-								<Link key={event.id} to={`/events/${event.id}`}>
-									<S.Preview key={event.id}>{event.title}</S.Preview>
-								</Link>
-							))}
-					</Panel>
-					<Panel title="All Events">
-						{this.props.events.map(event => (
-							<Link key={event.id} to={`/events/${event.id}`}>
-								<S.Preview key={event.id}>{event.title}</S.Preview>
-							</Link>
-						))}
-					</Panel>
-				</Tabs>
-			</S.DashBoardContainer>
-		);
-	}
+                                        {/* Drop Down Schedule Code start*/}
+                                        <div className={classes.root}>
+                                            <ExpansionPanel expanded={props.expanded}>
+                                                <ExpansionPanelSummary
+                                                    expandIcon={<ExpandMoreIcon />}
+                                                    id={`${props.id}_header`}
+                                                >
+                                                    <Typography className={classes.heading}>{props.title}</Typography>
+                                                </ExpansionPanelSummary>
+                                                <ExpansionPanelDetails>
+                                                    <div>
+                                                        {props.schedule &&
+                                                            props.schedule.length > 0 &&
+                                                            props.schedule.map(schd => {
+                                                                let date = new Date(schd.start_date_time);
+                                                                return (
+                                                                    <ScheduleWrapper>
+                                                                        <span>{`${schd.location} ${moment(date).format(
+                                                                            "MMMM Do YYYY, h:mm:ss a"
+                                                                        )}`}</span>
+                                                                    </ScheduleWrapper>
+                                                                );
+                                                            })}
+                                                    </div>
+                                                </ExpansionPanelDetails>
+                                            </ExpansionPanel>
+                                        </div>
+                                        {/* Drop Down Schedule Code end*/}
+
+
+                                    </S.Preview>
+                                </Link>
+                            ))}
+                    </Panel>
+                    <Panel title="Influencer Events">
+                        {this.props.events
+                            .filter(event =>
+                                this.props.subscriptions
+                                    .map(sub => sub.influencer_id)
+                                    .includes(event.host_id)
+                            )
+                            .map(event => (
+                                <Link key={event.id} to={`/events/${event.id}`}>
+                                    <S.Preview key={event.id}>{event.title}</S.Preview>
+                                </Link>
+                            ))}
+                    </Panel>
+                    <Panel title="All Events">
+                        {this.props.events.map(event => (
+                            <Link key={event.id} to={`/events/${event.id}`}>
+                                <S.Preview key={event.id}>{event.title}</S.Preview>
+                            </Link>
+                        ))}
+                    </Panel>
+                </Tabs>
+
+                <CEButton onClick={e => this.props.history.push("createEvent")}>
+                    Create New Event
+							</CEButton>
+
+            </S.DashBoardContainer>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-	return {
-		events: state.eventReducer.events,
-		subscriptions: state.subscriptionReducer.subscriptions
-	};
+    return {
+        events: state.eventReducer.events,
+        subscriptions: state.subscriptionReducer.subscriptions
+    };
 };
 
 export default connect(
-	mapStateToProps,
-	{ fetchEvents, getSubscriptionsByUserId }
+    mapStateToProps,
+    { fetchEvents, getSubscriptionsByUserId }
 )(EventsDashboard);
 
 
-// INstructions
-// It needs the following:
-// -A create new event button
-// -Some type of drop down that shows a schedule list underneath the event item (something similar to the attached screenshot)
+// Instructions
+// - Event Button: Create new event button
+// - Drop Down Schedule: Drop down from eventDetails that shows a schedule list underneath the event item similar to eventDetials in home page
