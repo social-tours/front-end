d509046685d2a1a2ce1cf4e7b5405be607f9db9f
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-06 15:11:10 +0800

    Refactor fetch all schedules action creator
    Add fetch individual schedule action creator
    
    Refactor add schedule action creator
    
    Add delete schedule action creator
    
    Add update schedule action creator

--- socialtours/src/actions/scheduleActions.js
+++ socialtours/src/actions/scheduleActions.js
@@ -1,69 +1,127 @@
 import axios from "axios";
 import { API_ENDPOINT } from "../config/api";
 
-import { getEvents } from "./events";
-
-const SCHEDULES = "/api/schedules";
-
 const SUCCESS = 200;
 
 export const types = {
+	// GET ALL
+	FETCH_ALL_SCHEDULES_START: "FETCH_ALL_SCHEDULES_START",
+	FETCH_ALL_SCHEDULES_FAILED: "FETCH_ALL_SCHEDULES_FAILED",
+	FETCH_ALL_SCHEDULES_SUCCESS: "FETCH_ALL_SCHEDULES_SUCCESS",
+
 	// GET
-	FETCHING_SCHEDULE: "FETCHING_SCHEDULE",
+	FETCH_SCHEDULE_START: "FETCH_SCHEDULE_START",
 	FETCH_SCHEDULE_FAILED: "FETCH_SCHEDULE_FAILED",
-	FETCH_SCHEDULE_SUCCESS: "FETCG_SCHEDULE_SUCCESS",
+	FETCH_SCHEDULE_SUCCESS: "FETCH_SCHEDULE_SUCCESS",
+
 	// POST
-	INSERTING_SCHEDULE: "INSERTING_SCHEDULE",
+	INSERT_SCHEDULE_START: "INSERT_SCHEDULE_START",
 	INSERT_SCHEDULE_FAILED: "INSERT_SCHEDULE_FAILED",
 	INSERT_SCHEDULE_SUCCESS: "INSERT_SCHEDULE_SUCCESS",
 	// PUT
-	UPDATING_SCHEDULE: "UPDATING_SCHEDULE",
+	UPDATE_SCHEDULE_START: "UPDATE_SCHEDULE_START",
 	UPDATE_SCHEDULE_FAILED: "UPDATE_SCHEDULE_FAILED",
 	UPDATE_SCHEDULE_SUCCESS: "UPDATE_SCHEDULE_SUCCESS",
 	// DELETE
-	DELETING_SCHEDULE: "DELETING_SCHEDULE",
+	DELETE_SCHEDULE_START: "DELETE_SCHEDULE_START",
 	DELETE_SCHEDULE_FAILED: "DELETE_SCHEDULE_FAILED",
 	DELETE_SCHEDULE_SUCCESS: "DELETE_SCHEDULE_SUCCESS"
 };
 
-export const getSchedules = () => async dispatch => {
-	dispatch({ type: types.FETCHING_SCHEDULE });
+export const getAllSchedules = () => async dispatch => {
+	dispatch({ type: types.FETCH_ALL_SCHEDULES_START });
 	try {
-		const schedules = await axios.get(API_ENDPOINT + SCHEDULES);
+		const schedules = await axios.get(`${API_ENDPOINT}/api/schedules`);
 		if (schedules.status === SUCCESS) {
 			dispatch({
-				type: types.FETCH_SCHEDULE_SUCCESS,
+				type: types.FETCH_ALL_SCHEDULES_SUCCESS,
 				payload: schedules.data
 			});
-		} else {
-			dispatch({
-				type: types.FETCH_SCHEDULE_FAILED
-			});
-		}
+		} else throw err
 	} catch (err) {
 		console.log(err);
+		dispatch({
+			type: types.FETCH_ALL_SCHEDULES_FAILED,
+			payload: err
+		});
 	}
 };
+
+export const getSchedule = id => async dispatch => {
+	dispatch({ type: FETCH_SCHEDULE_START })
+	try {
+		const schedule = await axios.get(`${API_ENDPOINT}/api/schedules/${id}`);
+		if (schedule.status === SUCCESS) {
+			dispatch({
+				type: types.FETCH_SCHEDULE_SUCCESS,
+				payload: schedule.data
+			});
+		} else throw err
+	} catch (err) {
+		console.log(err);
+		dispatch({
+			type: types.FETCH_SCHEDULE_FAILED,
+			payload: err
+		});
+	}
+}
 
 export const addSchedule = schedule => async dispatch => {
 	dispatch({ type: types.INSERTING_SCHEDULE });
 	try {
-		const res = await axios.post(API_ENDPOINT + SCHEDULES, schedule);
+		const res = await axios.post(`${API_ENDPOINT}/api/schedules`, schedule);
 		console.log("Adding schedule", schedule, res);
 		if (res.status === SUCCESS) {
 			dispatch({
-				type: types.INSERT_SCHEDULE_SUCCESS
+				type: types.INSERT_SCHEDULE_SUCCESS,
+				payload: res.data
 			});
-			getEvents();
-		} else {
-			dispatch({
-				type: types.INSERT_SCHEDULE_FAILED
-			});
-		}
+		} else throw err
 	} catch (err) {
 		console.log(err);
+		dispatch({
+			type: types.FETCH_SCHEDULE_FAILED,
+			payload: err
+		});
 	}
 };
+
+export const updateSchedule = schedule => async dispatch => {
+	dispatch({ type: UPDATE_SCHEDULE_START })
+	try {
+		const res = await axios.get(`${API_ENDPOINT}/api/schedules/${schedule.id}`, schedule);
+		if (res.status === SUCCESS) {
+			dispatch({
+				type: types.UPDATE_SCHEDULE_SUCCESS,
+				payload: res.data
+			});
+		} else throw err
+	} catch (err) {
+		console.log(err);
+		dispatch({
+			type: types.UPDATE_SCHEDULE_FAILED,
+			payload: err
+		});
+	}
+}
+
+export const deleteSchedule = id => async dispatch => {
+	dispatch({ type: DELETE_SCHEDULE_START })
+	try {
+		const schedule = await axios.delete(`${API_ENDPOINT}/api/schedules/${id}`);
+		if (schedule.status === SUCCESS) {
+			dispatch({
+				type: types.DELETE_SCHEDULE_SUCCESS
+			});
+		} else throw err
+	} catch (err) {
+		console.log(err);
+		dispatch({
+			type: types.DELETE_SCHEDULE_FAILED,
+			payload: err
+		});
+	}
+}
 
 // Shape of schedule
 //
