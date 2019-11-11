31880de3466f4098ecdff58697bc21ca723bbda6
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-11 12:49:58 +0800

    Finalize payment form design

--- socialtours/src/components/PaymentComponents/PaymentComponentStyles.js
+++ socialtours/src/components/PaymentComponents/PaymentComponentStyles.js
@@ -8,20 +8,96 @@ import {
 	breakpoints
 } from "../DesignComponents/theme";
 import Button from "../DesignComponents/Button";
-import * as S from "../DesignComponents/FormStyles";
+import InputWrapper from "../EventComponents/EventComponentsStyles"
 
 export const PaymentContainer = styled.div`
-	width: 600px;
+	width: 90%;
+	max-width: 500px;
 	${flex("column", "center", "center")};
 	margin: 50px auto 10px auto;
+	padding: 20px;
+	background: ${color.secondaryBgShading};
 
-	.stripe-card-element {
+	form {
 		width: 100%;
-		padding: 10px;
-		border-radius: 5px;
-		border: 1px solid ${colorScheme.secondaryBorderColor};
+		${flex("column", "space-between", "center")};
+		padding: 0 10px;
+		border: 2px solid ${colors.grape};
+		border-radius: 10px;
+		background: ${color.lightText};
+
+		h3 {
+			margin-top: 20px;
+			margin-bottom: 10px;
+			font-size: ${fontSizing.sm};
+			font-weight: bold;
+			text-transform: uppercase;
+		}
+
+		input {
+			width: 80%;
+			height: 35px;
+			padding-left: 10px;
+			border-radius: 5px;
+			border: 1px solid ${colorScheme.secondaryBorderColor};
+			font-size: ${fontSizing.m};
+
+			@media ${breakpoints[1]} {
+				width: 100%;
+				padding: 10px;
+			}
+		}
+
+		.product-description, .product-price {
+			width: 80%;
+			font-size: ${fontSizing.m};
+		}
+
+		.amount {
+			font-size: ${fontSizing.ml};
+			font-weight: bold;
+		}
+
+		.stripe-card-element {
+			width: 100%;
+			padding: 10px;
+			border-radius: 5px;
+			border: 1px solid ${colorScheme.secondaryBorderColor};
+		}
+
+		button {
+			background-color: ${colors.red};
+			color: ${color.lightText};
+			height: 35px;
+			width: 85%;
+			max-width: 150px;
+			margin: 25px auto;
+			border: 1px solid transparent;
+			border-radius: 25px;
+			font-size: ${fontSizing.s};
+			font-weight: bold;
+			text-transform: uppercase;
+			cursor: pointer;
+
+			@media ${breakpoints[0]} {
+				font-size: ${fontSizing.xxs}
+			}		
 	}
+
 `;
+
+export const PayInputWrapper = styled(InputWrapper)`
+	${flex("row", "center", "space-between")};
+	margin: 5px 0;
+	padding: 0;
+	background: ${color.lightText};
+
+	label {
+		width: 15%;
+	}
+
+`
+
 export const PayButton = styled(Button)`
 	width: 250px;
 	padding: 10px;
--- socialtours/src/components/PaymentComponents/PaymentForm.js
+++ socialtours/src/components/PaymentComponents/PaymentForm.js
@@ -1,46 +1,137 @@
 import React, { Component } from "react";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { CardElement, injectStripe } from "react-stripe-elements";
 
 import * as S from "./PaymentComponentStyles";
+import { API_ENDPOINT } from "../../config/api";
 
 class PaymentForm extends Component {
-	// constructor(props) {
-	//   super(props)
-
-	//   this.state = {
-	//     name: "",
-	//     amount: ""
-	//   };
-	// }
-
 	state = {
-		name: "",
-		amount: ""
+		description: "",
+		price: 5,
+		quantity: 0,
+		amount: 0,
+		coupon: '',
+		email: '',
+		name: '',
+		address: '',
+		city: '',
+		state: '',
+		country: '',
+		postal_code: ''
 	};
+
+
 
 	handleInput = e => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 
+	handlePricing = e => {
+		//this.handleInput(e)
+		this.setState({ [e.target.name]: e.target.value }, () => {
+			this.setState({ amount: this.state.price * this.state.quantity });
+		})
+	}
+
 	handlePayment = async () => {
 		let { token } = await this.props.stripe.createToken({
-			name: this.state.name
+			name: this.state.name,
+			address_line1: this.state.address,
+			address_city: this.state.city,
+			address_zip: this.state.postal_code,
+			address_country: this.state.country
 		});
-		let response = await axios.post("/charge", token.id);
 
-		if (response.ok) console.log("Purchase complete!");
+		console.log("STRIPE TOKEN STATUS: ", token)
+
+		const amount = Number(this.props.amount) * 100;
+		const transactionDetails = {
+			description: this.state.description,
+			user_id: jwt_decode(localStorage.getItem("api_token")).id,
+			// event_schedule_id: this.props.schedule.id,
+			amount,
+			token
+		};
+
+		try {
+			const response = await axios.post(
+				`${API_ENDPOINT}/api/sales`,
+				transactionDetails
+			);
+			console.log(response);
+			alert("Payment Success");
+		} catch (error) {
+			console.log("Payment Error: ", error);
+			alert("Payment Error");
+		}
 	};
 
 	render() {
 		console.log("THIS.PROPS: ", this.props);
+		
 		return (
 			<S.PaymentContainer>
-				<CardElement
-					className="stripe-card-element"
-					style={{ base: { fontSize: "18px" } }}
-				/>
-				<S.PayButton>Purchase</S.PayButton>
+				<form>
+					<h3>Order Information</h3>
+					<div className="product-description">{this.state.description}</div>
+					<S.PayInputWrapper>
+						<label htmlFor="price">Price</label>
+						<div className="product-price">
+							{
+								(this.state.price).toLocaleString('en-US', { style: 'currency', currency: 'usd' })
+							}
+						</div>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="quantity">Quantity</label>
+						<input name="quantity" onChange={this.handlePricing} type="number"/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label className="amount" htmlFor="amount">Total</label>
+						<div className="amount">
+							{
+								(this.state.amount).toLocaleString('en-US', { style: 'currency', currency: 'usd' })
+							}
+						</div>
+					</S.PayInputWrapper>
+					<h3>Billing Information</h3>
+					<S.PayInputWrapper>
+						<label htmlFor="name">Name</label>
+						<input name="name" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="email">Email</label>
+						<input name="email" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="address">Address</label>
+						<input name="address" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="city">City</label>
+						<input name="city" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="">State</label>
+						<input name="state" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="">Postal Code</label>
+						<input name="postal_code" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<S.PayInputWrapper>
+						<label htmlFor="country">Country</label>
+						<input name="country" onChange={this.handleInput} placeholder=""/>
+					</S.PayInputWrapper>
+					<h3>Payment Information</h3>
+					<CardElement
+						className="stripe-card-element"
+						style={{ base: { fontSize: "18px" } }}
+					/>
+					<S.PayButton>Purchase</S.PayButton>
+				</form>
 			</S.PaymentContainer>
 		);
 	}
