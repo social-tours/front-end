f21cf6762f83b000e475da12503d03c94b41bd0c
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-05-09 10:24:28 +0800

    Update production domain name
    Add .app address extension

--- socialtours/src/config/api.js
+++ socialtours/src/config/api.js
@@ -8,14 +8,25 @@ if (hostname === "www.socialtours.io") {
 	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
 	// API_ENDPOINT = "https://prod-socialtours.herokuapp.com";
 	REDIRECT_URI = "https://www.socialtours.io";
+} else if (hostname === "https://socialtours.netlify.com") {
+	REDIRECT_URI = "https://socialtours.netlify.com";
+} else if (hostname === "https://socialtours.netlify.app") {
+	REDIRECT_URI = "https://socialtours.netlify.app";
 } else if (hostname === "staging-a-socialtours.netlify.com") {
 	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
 	// API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
-	REDIRECT_URI = "https://staging-a-socialtours.netlify.com";
+} else if (hostname === "staging-a-socialtours.netlify.app") {
+	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
+	// API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
+	REDIRECT_URI = "https://staging-a-socialtours.netlify.app";
 } else if (hostname === "staging-b-socialtours.netlify.com") {
 	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
 	// API_ENDPOINT = "https://staging-b-socialtours.herokuapp.com";
 	REDIRECT_URI = "https://staging-b-socialtours.netlify.com";
+} else if (hostname === "staging-b-socialtours.netlify.app") {
+	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
+	// API_ENDPOINT = "https://staging-b-socialtours.herokuapp.com";
+	REDIRECT_URI = "https://staging-b-socialtours.netlify.app";
 } else {
 	API_ENDPOINT = "https://staging-a-socialtours.herokuapp.com";
 	// API_ENDPOINT = "http://localhost:8080";
